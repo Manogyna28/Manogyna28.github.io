7165ef6d35950ffdfda380cab00b8cd8c1539388
Manogyna28 <arukalamanogyna@gmail.com>
2026-01-10 12:13:32 +0530

    update

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -14,20 +14,22 @@ function dispMessage(){
         lblMessage.innerHTML="access denied";
      }
 }
-function ShowLoginForm(){
-    let str=`
-       <h3>Login Form</h3>
-        <p><label id="lblMessage"></label></p>
-        <p><label id="lblMessage1"></label></p>
-        <p><input type="text" id="txtEmail"></p>
-        <p><input type="password" id="txtPassword"></p>
-        <p><button class="login-btn" onclick="dispMessage()">Log In</button></p>
-        <hr>
-        <p><button class="account" onclick="showResgisterForm()">Create Account</button></p>
+function showLoginForm(){
+    let str = `
+    <h3>Login Form</h3>
+      <p><label id="lblMessage"></label></p>
+      <p><input type="text" id="txtEmail" /></p>
+      <p><input type="password" id="txtPassword" /></p>
+      <p><button class="login-btn" onclick="dispMessage()">Log In</button></p>
+      <hr>
+      <p><button class="account" onclick="showRegisterForm()">Create Account</button></p>
     `
-    root.innerHtml=str
+    root.innerHTML = str
 }
+
 function showRegisterForm(){
-   let str=
-   let s
+    let str =  `
+     <h3>Registration Form</h3>
+    `
+    root.innerHTML = str
 }
